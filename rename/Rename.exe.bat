fef5d24e403be0ab8378166c39dfd7ae1fe55135
Victoria Cadogan <45957062+Victrollia@users.noreply.github.com>
2025-05-04 14:49:07 -0400

    Update Rename.exe.bat

--- rename/Rename.exe.bat
+++ rename/Rename.exe.bat
@@ -1,5 +1,13 @@
 @ECHO OFF
-md upload
-md processed
+if exist upload\ (
+	echo Please add images to upload folder
+) else (
+	md upload
+)
+if exist processed\ (
+	echo: 
+) else (
+	md processed
+)
 python rename.py
 pause
