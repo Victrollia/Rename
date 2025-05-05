9185779ae19a67b682d7761914f41ee146aeef63
Victoria Cadogan <45957062+Victrollia@users.noreply.github.com>
2025-05-05 10:15:18 -0400

    Update rename.py
    Fixed minor errors regarding string and index formats for the index input.

--- rename/rename.py
+++ rename/rename.py
@@ -9,13 +9,15 @@ def main():
         img_name = img_name.replace(" ","-")
         img_name = img_name.replace("'","")
         i = 1
+        index = int(index)
         old_path = os.getcwd() + r'\\upload\\'
         new_path = os.getcwd() + r'\\processed\\'
         for filename in os.listdir(old_path):
             old_name = old_path + filename
-            new_name = new_path + img_name + '_0' + index + '.jpg'
+            new_name = new_path + img_name + '_0' + str(index) + '.jpg'
             os.rename(old_name, new_name)
             i += 1
+            index +=1
         print(str(i-1) + " images got renamed.\n\n")
 
 
