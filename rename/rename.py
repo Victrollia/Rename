61b28b0e9dc54c07b3e20a18604541312d556f7b
Victoria Cadogan <45957062+Victrollia@users.noreply.github.com>
2025-05-05 09:33:39 -0400

    Update rename.py
    added index input so if image(s) already existed on the website, to start the count at whatever index the user enters.

--- rename/rename.py
+++ rename/rename.py
@@ -4,6 +4,7 @@ def main():
     while True:
         vendor = input("Manufacturer:")
         mpn = input("MPN:")
+        index = input("Enter the index of the image (for example 1 for _01):")
         img_name = vendor.lower() + '-' + mpn.lower()
         img_name = img_name.replace(" ","-")
         img_name = img_name.replace("'","")
@@ -12,7 +13,7 @@ def main():
         new_path = os.getcwd() + r'\\processed\\'
         for filename in os.listdir(old_path):
             old_name = old_path + filename
-            new_name = new_path + img_name + '_0' + str(i) + '.jpg'
+            new_name = new_path + img_name + '_0' + index + '.jpg'
             os.rename(old_name, new_name)
             i += 1
         print(str(i-1) + " images got renamed.\n\n")
